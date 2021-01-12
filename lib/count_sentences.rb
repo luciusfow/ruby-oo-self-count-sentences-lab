2a42ffaf0a9b91f67e6c99baf03d4bd543d4b5d5
Lucius Fowler <luciusfow@gmail.com>
2021-01-11 20:27:08 -0500

    commit tree 0dc7d101f52e8987bfcb6c169cf2fca023e66cff

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    # array = self.split(".,?")
+#     array = self.split(/[.?]/)
+#     array.length
+#     array.count
+#  binding.pry
+  self.split(/\.|\?|\!/).delete_if{|what|what.length <1.5 }.count
   end
 end
